f570c1fc37d5e5276754fa86359b862341d29ec3
anewla01 <andre.newland@tufts.edu>
2024-08-15 13:55:00 -0400

    WIP: updated system script

--- scripts/system_resources_test.sh
+++ scripts/system_resources_test.sh
@@ -23,8 +23,9 @@ function main(){
   local cpu_util=$(get_cpu_utiliztion_pct)
   echo "Found CPU usage: ${cpu_util}% (THREHOLD: ${CPU_UTILIZATION_THRESHOLD}%)"
 
-  if [[ $(echo "${cpu_util}" > "${CPU_UTILIZATION_THRESHOLD}" | bc) -ne 0 ]]
+  if [[ $(echo "${cpu_util} > ${CPU_UTILIZATION_THRESHOLD}" | bc) -eq 1 ]]
   then
+    echo "***CPU higher than threshold***"
     exit 1
   fi
 }
